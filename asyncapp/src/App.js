c2547558a1ab001a154ae793f28c2a03a41de464
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-02-19 14:42:50 +0000

    finished mvp

--- asyncapp/src/App.js
+++ asyncapp/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import DogFinder from './components/DogFinder';
+import styled from 'styled-components';
+
+const H2 = styled.h2 `
+font-family: monospace;
+
+`
+
+
 
 function App() {
   return (
     <div className="App">
- 
+      <H2> Dog Image Finder! </H2>
+      <DogFinder />
     </div>
   );
 }
--- asyncapp/src/index.js
+++ asyncapp/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './components/reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
 
 
